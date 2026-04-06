65af2c0e4409b547010db1dc6cfc4a3f54e07ca5
Seriyye Ceferova <seriw1999@gmail.com>
2026-04-07 03:23:29 +0400

    Update API_BASE_URL to production endpoint

--- Assets/Js/Core/Config.js
+++ Assets/Js/Core/Config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    API_BASE_URL: 'https://localhost:7128/api',
+
+    API_BASE_URL: 'https://avenora-api.onrender.com/api', 
     ENDPOINTS: {
         AUTH: '/Auth',
         TOURS: '/Tours',
